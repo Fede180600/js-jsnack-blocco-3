a82220231f33832a5f245caefca4057c555a33f0
Fede180600 <fede180600@libero.it>
2022-05-04 14:53:19 +0200

    pseudo-code

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,2 +1,5 @@
 // JSNACK2. Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando a somma degli elementi è minore di 50.
 
+// Creo array di numeri utente
+
+ // Ciclo while per chiedere numeri epusharli nella array userNums finche la somma è < 50 
