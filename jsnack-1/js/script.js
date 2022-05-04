873d42c2bb231708b2cb7ae328ebafeafe8b328f
Fede180600 <fede180600@libero.it>
2022-05-04 13:51:16 +0200

    pseudo-code

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,1 +1,8 @@
-// JSNACK1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+// JSNACK1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanto l’altro.
+
+// Creo le due array 
+
+// Condizione/While per confrontare le due array
+// - se una delle due è più corta
+    // funzione x generare numero random da pushare nella array più corta
+// - se sono uguali non faccio nulla
