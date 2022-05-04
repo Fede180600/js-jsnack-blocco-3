8d1461a695d060cc7e20a7c15fdf12243017313f
Fede180600 <fede180600@libero.it>
2022-05-04 14:34:35 +0200

    jsnack1 done

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,8 +1,20 @@
 // JSNACK1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanto l’altro.
 
 // Creo le due array 
+const firstArray = [23, 97, 45, 13, 56, 74, 15, 134, 8];
+const secondArray = [12, 3, 53, 27];
 
-// Condizione/While per confrontare le due array
-// - se una delle due è più corta
-    // funzione x generare numero random da pushare nella array più corta
-// - se sono uguali non faccio nulla
+
+// While per confrontare le due array
+do {
+    const newNum = Math.floor(Math.random() * 200) + 1;
+    if (firstArray.length > secondArray.length) {
+        secondArray.push(newNum);
+    } else if (secondArray.length > firstArray.length) {
+        firstArray.push(newNum);
+    }
+} while (firstArray.length >  secondArray.length || secondArray.length > firstArray.length);
+
+console.log(firstArray);
+console.log(secondArray);
+
