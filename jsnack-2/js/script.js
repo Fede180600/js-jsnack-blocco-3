e296eeb1194826cc840ea3a3e5bec9da3f3bc0f6
Fede180600 <fede180600@libero.it>
2022-05-04 16:14:54 +0200

    tring to do sum

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -2,13 +2,21 @@
 
 // Creo array di numeri utente
 const userNums = [];
-console.log(userNums);
+
 let numsSum = 0;
-// Ciclo per fare la somma dei numeri dell'array
-for (let i = 0; i < userNums.length; i++) {
-    const num = userNums[i];
-    numsSum += num;
-    console.log("la somma è:", numsSum);
-}
- // Ciclo while per chiedere numeri e pusharli nella array userNums finche la somma è < 50 
- 
+// Condizione per chiedere numeri e pusharli nella array userNums finche la somma è < 50 
+while (numsSum < 50) {
+    if (numsSum < 50) {
+        const userNum = parseInt(prompt("inserisci un numero"));
+        console.log("Numero inserito dall'utente:", userNum);
+        if (numsSum < 50) {
+            // Ciclo per fare la somma dei numeri dell'array
+            for (let i = 0; i < userNums.length; i++) {
+                const num = userNums[i];
+                numsSum += num;
+                console.log("la somma è:", numsSum);
+            };
+            userNums.push(userNum)
+        };
+    };
+} 
