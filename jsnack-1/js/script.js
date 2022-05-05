e12efb655a055cc8ea7a0200e046b92411f6d395
Fede180600 <fede180600@libero.it>
2022-05-05 09:57:16 +0200

    jsnack1 correction

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,20 +1,43 @@
 // JSNACK1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanto l’altro.
 
 // Creo le due array 
+// const firstArray = [23, 97, 45, 13, 56, 74, 15, 134, 8];
+// const secondArray = [12, 3, 53, 27];
+
+
+// // While per confrontare le due array
+// do {
+//     const newNum = Math.floor(Math.random() * 200) + 1;
+//     if (firstArray.length > secondArray.length) {
+//         secondArray.push(newNum);
+//     } else if (secondArray.length > firstArray.length) {
+//         firstArray.push(newNum);
+//     }
+// } while (firstArray.length >  secondArray.length || secondArray.length > firstArray.length);
+
+// console.log(firstArray);
+// console.log(secondArray);
+
+// CORREZIONE
+// Creo le due array 
 const firstArray = [23, 97, 45, 13, 56, 74, 15, 134, 8];
 const secondArray = [12, 3, 53, 27];
 
+let shortArray; 
 
-// While per confrontare le due array
-do {
-    const newNum = Math.floor(Math.random() * 200) + 1;
-    if (firstArray.length > secondArray.length) {
-        secondArray.push(newNum);
-    } else if (secondArray.length > firstArray.length) {
-        firstArray.push(newNum);
-    }
-} while (firstArray.length >  secondArray.length || secondArray.length > firstArray.length);
+if (firstArray.length > secondArray.length) {
+    shortArray = secondArray;
+} else {
+    shortArray = firstArray;
+}
 
-console.log(firstArray);
-console.log(secondArray);
+while(firstArray.length !== secondArray.length) {
+    const rndNumber = getRndInteger(1, 100);
+    shortArray.push(rndNumber);
+}
 
+console.log(firstArray, secondArray);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
